f5c7f12ad8eb471157a7b4397985f4d634411591
nick <nicktulett@gmail.com>
2025-01-19 16:44:06 +0000

    Use pagemodel and testdata

---
+++ chartpage.py
@@ -0,0 +1,12 @@
+import re
+
+
+class ChartPage:
+    def __init__(self, page):
+        self.page = page
+        self.chart_frame = page.locator("iframe").content_frame
+        self.first_chart_plots = self.chart_frame.get_by_label(re.compile("Actual CfD")).first.get_by_label("Plot area")
+
+    def navigate(self):
+        self.page.goto("https://www.lowcarboncontracts.uk/resources/scheme-dashboards/cfd-historical-data-dashboard/"
+)
--- test_powerbi.py
+++ test_powerbi.py
@@ -1,30 +1,38 @@
 import re
 from playwright.sync_api import Page, expect
 import pytest
+from chartpage import ChartPage
 
 
-# def test_has_title(page: Page):
-#     page.goto("https://www.lowcarboncontracts.uk/resources/scheme-dashboards/cfd-historical-data-dashboard/")
-#
-#     # Expect a title "to contain" a substring.
-#     expect(page).to_have_title(re.compile("CfD Historical Data"))
+check_values = {
+    "Offshore Wind": "773975.067"
+}
+
+@pytest.fixture(scope="function", autouse=True)
+def chart_page(page: Page):
+    chart_page = ChartPage(page)
+    chart_page.navigate()
+    yield chart_page
 
 
-def test_has_charts(page: Page):
-    page.goto("https://www.lowcarboncontracts.uk/resources/scheme-dashboards/cfd-historical-data-dashboard/")
-    chart_frame = page.locator("iframe").content_frame
+def test_has_title(chart_page):
+    expect(chart_page.page).to_have_title(re.compile("CfD Historical Data"))
 
-    expect(chart_frame.get_by_label(re.compile("Actual CfD"))).to_have_count(2)
-    expect(chart_frame.get_by_text("2025 Qtr 1")).to_have_count(2)
 
-    first_chart_plots = chart_frame.get_by_label(re.compile("Actual CfD")).first.get_by_label("Plot area")
-    expect(first_chart_plots).to_have_count(2)
+def test_has_two_charts(chart_page):
+    expect(chart_page.chart_frame.get_by_label(re.compile("Actual CfD"))).to_have_count(2)
+    expect(chart_page.chart_frame.get_by_text("2025 Qtr 1")).to_have_count(2)
+    expect(chart_page.first_chart_plots).to_have_count(2)
+
+def test_chart_has_all_metrics(chart_page):
     # chart has 7 bar stacks per date
-    expect(first_chart_plots.first.get_by_role("listbox")).to_have_count(7)
+    expect(chart_page.first_chart_plots.first.get_by_role("listbox")).to_have_count(7)
     # chart has 3 overlaid lines
-    expect(first_chart_plots.last.locator("path")).to_have_count(3)
+    expect(chart_page.first_chart_plots.last.locator("path")).to_have_count(3)
 
+def test_chart_values(chart_page):
+    for metric, value in check_values.items():
     # check an Offshore wind value
-    (expect(first_chart_plots.first
-           .get_by_label("Offshore Wind").get_by_role("option").last)
-                .to_have_attribute("aria-label", "773975.067"))
+        (expect(chart_page.first_chart_plots.first
+            .get_by_label(metric).get_by_role("option").last)
+                .to_have_attribute("aria-label", value))
