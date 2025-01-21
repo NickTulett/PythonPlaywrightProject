4f99ec3c1cd38c04c05c572247d319e1c5f0f108
Nick Tulett <nicktulett@gmail.com>
2025-01-21 10:35:13 +0000

    use pytest_check to continue looping beyond test failures

--- test_faq_links.py
+++ test_faq_links.py
@@ -1,5 +1,6 @@
 import pytest
 from playwright.sync_api import Page
+from pytest_check import check, is_not_none, is_false
 
 pages = [
     "https://www.lowcarboncontracts.uk/resources/faqs/supplier-cfd-rab-faqs/",
@@ -16,12 +17,11 @@ def test_links(page: Page, faq_page):
         for faq_link in faq_links.all():
             print('\nLINK TEXT:', faq_link.text_content())
             href = faq_link.get_attribute("href")
-            if href:
+            with check:
+                assert is_not_none(href, "MISSING LINK")
                 print("LINK HREF:", href)
                 page.goto(href)
-                if page.get_by_role("heading", name="Not Found").is_visible():
-                    print("BROKEN LINK!")
-                else: print("LINK OK")
-                page.go_back()
-            else: print("MISSING LINK!")
-
+                with check:
+                    assert is_false(page.get_by_role("heading", name="Not Found").is_visible(), "BROKEN LINK!")
+                    print("LINK OK")
+            page.go_back()
